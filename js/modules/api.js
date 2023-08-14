0b7da38ac6057c9603af5ef5007f2e51ec3f9a23
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-08-14 16:10:13 -0300

    mudando a baseUrl para a da api que foi feito o deploy

--- js/modules/api.js
+++ js/modules/api.js
@@ -1,5 +1,5 @@
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://sore-blue-wildebeest-gown.cyclic.cloud/",
   timeout: 1000,
   headers: {
     "Content-Type": "application/json",

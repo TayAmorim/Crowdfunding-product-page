a48e900579cb4409f1fcdcbfb124862f73432dd8
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-08-16 17:29:06 -0300

    ajuste na organização do codigo

--- js/modules/api.js
+++ js/modules/api.js
@@ -1,6 +1,6 @@
 export const api = axios.create({
   baseURL: "https://sore-blue-wildebeest-gown.cyclic.cloud/",
-  timeout: 1000,
+  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
--- js/modules/product-info.js
+++ js/modules/product-info.js
@@ -5,43 +5,60 @@ const informationText = document.querySelector(".information-text");
 const rangeValue = document.querySelector(".remaining-value");
 
 export default async function initInfoProduct() {
-  const response = await api.get(`/produto/1`);
-  const { meta_valor, valor_arrecadado, total_apoios, data_limite } =
-    response.data;
-
-  const formatterValue = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  });
-
-  const amountRaised = formatterValue.format(valor_arrecadado / 100);
-  const targetValue = formatterValue.format(meta_valor / 100);
-  const dayRemaining = dateFns.differenceInCalendarDays(
-    new Date(data_limite),
-    new Date()
-  );
-
-  titlesInformation.forEach((title) => {
-    if (title.dataset.informationTitle === "values") {
-      title.innerText = ` ${amountRaised}`;
-      informationText.innerText = `${targetValue}`;
-    } else if (title.dataset.informationTitle === "backed") {
-      title.innerText = ` ${total_apoios}`;
-    } else {
-      title.innerText = ` ${dayRemaining}`;
+  try {
+    const response = await api.get(`/produto/1`);
+    const { meta_valor, valor_arrecadado, total_apoios, data_limite } =
+      response.data || {};
+    function priceFormatting() {
+      const formatterValue = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+      const amountRaised = formatterValue.format(valor_arrecadado / 100);
+      const targetValue = formatterValue.format(meta_valor / 100);
+      return { amountRaised, targetValue };
     }
-  });
-  const valueRange = valor_arrecadado / 100;
-  const maxValueRange = meta_valor / 100;
 
-  rangeValue.setAttribute("max", maxValueRange);
+    function nextDays() {
+      const dayRemaining = dateFns.differenceInCalendarDays(
+        new Date(data_limite),
+        new Date()
+      );
+      return dayRemaining;
+    }
 
-  function updateBandColor() {
-    const percent =
-      ((valueRange - rangeValue.min) / (rangeValue.max - rangeValue.min)) * 100;
-    rangeValue.style.background = `linear-gradient(to right, #3cb4ac 0%, #3cb4ac ${percent}%, #ddd ${percent}%, #ddd 100%)`;
-    rangeValue.setAttribute("value", percent * 1000);
+    function manipulationText() {
+      titlesInformation.forEach((title) => {
+        if (title.dataset.informationTitle === "values") {
+          title.innerText = ` ${priceFormatting().amountRaised}`;
+          informationText.innerText = `${priceFormatting().targetValue}`;
+        } else if (title.dataset.informationTitle === "backed") {
+          title.innerText = ` ${total_apoios}`;
+        } else {
+          title.innerText = ` ${nextDays()}`;
+        }
+      });
+    }
+    manipulationText();
+    inputRangeManipulation();
+
+    function inputRangeManipulation() {
+      const valueRange = valor_arrecadado / 100;
+      const maxValueRange = meta_valor / 100;
+      rangeValue.setAttribute("max", maxValueRange);
+      updateBrandColor(valueRange);
+    }
+
+    function updateBrandColor(valueRange) {
+      const moderateCyan = "#3cb4ac";
+      const gray = "#ddd";
+      const percent =
+        ((valueRange - rangeValue.min) / (rangeValue.max - rangeValue.min)) *
+        100;
+      rangeValue.style.background = `linear-gradient(to right, ${moderateCyan} 0%, ${moderateCyan} ${percent}%, ${gray} ${percent}%, ${gray} 100%)`;
+      rangeValue.setAttribute("value", percent * 1000);
+    }
+  } catch (error) {
+    return console.log(error?.message);
   }
-
-  updateBandColor();
 }

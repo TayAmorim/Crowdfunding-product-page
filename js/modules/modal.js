d651481a9c006f94b1bb0f28edbd32adc0a59e0b
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-09-03 15:41:43 -0300

    :sparkles: feat : Mostrar detalhes do modal que faz um nova promessa

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -3,6 +3,7 @@ const supportButton = document.querySelector("[data-modal='btnSupport']");
 const rewardButtons = document.querySelectorAll(".btn");
 const buttonCloseModal = document.querySelector(".modal-img");
 const btnPledges = document.querySelectorAll(".btn-pledges");
+const modalSelect = document.querySelectorAll(".modal-select");
 
 export default function initModal() {
   supportButton.addEventListener("click", showModal);
@@ -12,9 +13,28 @@ export default function initModal() {
     button.addEventListener("click", openModalRewardSelected)
   );
 
+  btnPledges.forEach((pledge) =>
+    pledge.addEventListener("click", () => {
+      modalSelect.forEach((modal) => {
+        modal.classList.add("doNotShow");
+      });
+      const modalDetailDiv = modalPlans.querySelector(
+        `[data-plan=${pledge.id}]`
+      );
+      const modalDetail = modalDetailDiv.querySelector(".modal-select");
+      if (pledge.checked) {
+        modalDetail.classList.remove("doNotShow");
+      }
+    })
+  );
+
   function showModal() {
     btnPledges.forEach((btn) => {
       btn.checked = false;
+    });
+
+    modalSelect.forEach((modal) => {
+      modal.classList.add("doNotShow");
     });
     modalPlans.classList.remove("doNotShow");
     const scrollModal = modalPlans.scrollHeight;
@@ -28,9 +48,20 @@ export default function initModal() {
   function openModalRewardSelected({ target }) {
     const btnPlan = target.dataset.btnReward;
     showModal();
+    showModalDetails(btnPlan);
+  }
+
+  function showModalDetails(element) {
+    const modalDetailDiv = modalPlans.querySelector(`[data-plan=${element}]`);
+    const modalDetail = modalDetailDiv.querySelector(".modal-select");
+
     btnPledges.forEach((btn) => {
-      if (btn.id === btnPlan) {
+      if (btn.id === element) {
         btn.checked = true;
+        modalDetail.classList.remove("doNotShow");
+      }
+      if (btn.checked && btn.id != element) {
+        modalDetail.classList.add("doNotShow");
       }
     });
   }

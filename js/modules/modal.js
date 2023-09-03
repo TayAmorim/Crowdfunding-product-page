2e5c5532b90045a530715314098274879dde3eb8
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-09-03 14:20:18 -0300

    :sparkles: feat: Função abrir modal com a recompensa selecionada

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -2,15 +2,20 @@ const modalPlans = document.querySelector("[data-modal='plans']");
 const supportButton = document.querySelector("[data-modal='btnSupport']");
 const rewardButtons = document.querySelectorAll(".btn");
 const buttonCloseModal = document.querySelector(".modal-img");
+const btnPledges = document.querySelectorAll(".btn-pledges");
 
 export default function initModal() {
   supportButton.addEventListener("click", showModal);
   buttonCloseModal.addEventListener("click", doNotShowModal);
+
   rewardButtons.forEach((button) =>
-    button.addEventListener("click", showModal)
+    button.addEventListener("click", openModalRewardSelected)
   );
 
   function showModal() {
+    btnPledges.forEach((btn) => {
+      btn.checked = false;
+    });
     modalPlans.classList.remove("doNotShow");
     const scrollModal = modalPlans.scrollHeight;
     window.scrollTo(scrollModal, 0);
@@ -18,5 +23,15 @@ export default function initModal() {
 
   function doNotShowModal() {
     modalPlans.classList.add("doNotShow");
+  }
+
+  function openModalRewardSelected({ target }) {
+    const btnPlan = target.dataset.btnReward;
+    showModal();
+    btnPledges.forEach((btn) => {
+      if (btn.id === btnPlan) {
+        btn.checked = true;
+      }
+    });
   }
 }
